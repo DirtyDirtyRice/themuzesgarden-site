25e676f99f98419f9e191b4b459628117dc44f27
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-21 16:17:51 -0700

    Update MomentInspectorWorkspaceQueueBody.tsx

--- app/components/player/MomentInspectorWorkspaceQueueBody.tsx
+++ app/components/player/MomentInspectorWorkspaceQueueBody.tsx
@@ -5,13 +5,12 @@ import MomentInspectorWorkspaceContent from "./MomentInspectorWorkspaceContent";
 import MomentInspectorWorkspaceNoteSummaryBar from "./MomentInspectorWorkspaceNoteSummaryBar";
 import MomentInspectorWorkspaceQueueHeader from "./MomentInspectorWorkspaceQueueHeader";
 import MomentInspectorWorkspaceQueueStatsBar from "./MomentInspectorWorkspaceQueueStatsBar";
-import type { MomentInspectorWorkspacePanelViewModel } from "./momentInspectorWorkspacePanelViewModel";
 import type { MomentInspectorWorkspaceLane } from "./momentInspectorWorkspace.types";
 
 type MomentInspectorWorkspaceQueueBodyProps = {
   lane: MomentInspectorWorkspaceLane;
   searchQuery: string;
-  viewModel: MomentInspectorWorkspacePanelViewModel;
+  viewModel: any;
   selectedFamilyIds: string[];
   onToggleSelected: (familyId: string) => void;
 };
@@ -19,33 +18,36 @@ type MomentInspectorWorkspaceQueueBodyProps = {
 export default function MomentInspectorWorkspaceQueueBody(
   props: MomentInspectorWorkspaceQueueBodyProps
 ) {
+  const viewModel = props.viewModel ?? {};
+  const visibleStats = viewModel?.visibleStats ?? {};
+  const queueStats = visibleStats?.queueStats ?? {};
+  const noteSummary = visibleStats?.noteSummary ?? {};
+  const selectionSummary = viewModel?.selectionSummary ?? {};
+  const derivedView = viewModel?.derivedView ?? {};
+
   return (
     <div className="rounded-xl border border-zinc-200 bg-zinc-50 p-3">
       <MomentInspectorWorkspaceQueueHeader
         lane={props.lane}
-        visibleCount={props.viewModel.visibleStats.queueStats.visibleCount}
+        visibleCount={queueStats?.visibleCount ?? 0}
       />
 
       <div className="mt-3 space-y-3">
-        <MomentInspectorWorkspaceQueueStatsBar
-          stats={props.viewModel.visibleStats.queueStats}
-        />
+        <MomentInspectorWorkspaceQueueStatsBar stats={queueStats} />
 
-        <MomentInspectorWorkspaceNoteSummaryBar
-          summary={props.viewModel.visibleStats.noteSummary}
-        />
+        <MomentInspectorWorkspaceNoteSummaryBar summary={noteSummary} />
 
         <MomentInspectorWorkspaceBatchActionBar
           activeLane={props.lane}
-          selectedCount={props.viewModel.selectionSummary.selectedCount}
+          selectedCount={selectionSummary?.selectedCount ?? 0}
         />
 
         <MomentInspectorWorkspaceContent
           lane={props.lane}
           searchQuery={props.searchQuery}
-          derivedView={props.viewModel.derivedView}
-          selectedFamilyIds={props.selectedFamilyIds}
-          onToggleSelected={props.onToggleSelected}
+          derivedView={derivedView}
+          selectedFamilyIds={props.selectedFamilyIds ?? []}
+          onToggleSelected={props.onToggleSelected ?? (() => {})}
         />
       </div>
     </div>
