7715c01943c771daf4b0df6cfaf75a9375537fcf
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-21 13:31:33 -0700

    Update momentInspectorHostControlStack.types.ts

--- app/components/player/momentInspectorHostControlStack.types.ts
+++ app/components/player/momentInspectorHostControlStack.types.ts
@@ -1,10 +1,1 @@
-"use client";
-
-import MomentInspectorTrackControls from "./MomentInspectorTrackControls";
-import type { MomentInspectorHostControlStackProps } from "./momentInspectorHostControlStack.types";
-
-export default function MomentInspectorHostControlStack(
-  props: MomentInspectorHostControlStackProps
-) {
-  return <MomentInspectorTrackControls {...props} />;
-}
+export type MomentInspectorHostControlStackProps = Record<string, any>;
