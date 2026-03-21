cef2bb039d626d204806350dcc654448fe5baf3d
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-21 15:11:30 -0700

    Update MomentInspectorWorkspacePanelSectionRenderer.tsx

--- app/components/player/MomentInspectorWorkspacePanelSectionRenderer.tsx
+++ app/components/player/MomentInspectorWorkspacePanelSectionRenderer.tsx
@@ -28,12 +28,25 @@ export default function MomentInspectorWorkspacePanelSectionRenderer(
         groupMode={viewModel.groupMode}
         onSortModeChange={actions.onSortModeChange}
         onGroupModeChange={actions.onGroupModeChange}
+
         visibleCount={
           (visibleStats as any)?.queueStats?.visibleCount ??
           (visibleStats as any)?.visibleCount ??
           0
         }
+
         selectedCount={selectionSummary?.selectedCount ?? 0}
+
+        {/* ✅ REQUIRED PROPS (safe fallbacks) */}
+        laneSummaries={(viewModel as any)?.laneSummaries ?? []}
+        activeLane={(viewModel as any)?.activeLane ?? null}
+        onLaneChange={actions?.onLaneChange ?? (() => {})}
+        searchQuery={(viewModel as any)?.searchQuery ?? ""}
+        onSearchQueryChange={actions?.onSearchQueryChange ?? (() => {})}
+        filterMode={(viewModel as any)?.filterMode ?? "all"}
+        onFilterModeChange={actions?.onFilterModeChange ?? (() => {})}
+        groupBy={(viewModel as any)?.groupBy ?? "none"}
+        onGroupByChange={actions?.onGroupByChange ?? (() => {})}
       />
 
       <MomentInspectorWorkspacePanelQueueBlock context={context} />
