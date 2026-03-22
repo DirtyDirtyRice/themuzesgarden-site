ad671d12a92afbdf11db63c4399b05df96a1dddc
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-21 19:05:49 -0700

    Update momentInspectorHostTimelineStack.types.ts

--- app/components/player/momentInspectorHostTimelineStack.types.ts
+++ app/components/player/momentInspectorHostTimelineStack.types.ts
@@ -1,8 +1,8 @@
-import type { AnyRecord } from "./momentInspectorHostShared.types";
+export type AnyRecord = Record<string, any>;
 
 export type MomentInspectorHostTimelineStackProps = {
-  timelineProps: AnyRecord;
-  selectedProps: AnyRecord;
-  columnsProps: AnyRecord;
-  sectionsProps: AnyRecord;
+  timelineProps?: AnyRecord;
+  selectedProps?: AnyRecord;
+  columnsProps?: AnyRecord;
+  sectionsProps?: AnyRecord;
 };
