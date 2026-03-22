2fc4e2cfef48936230c71723ea6b8e3a3ade8de8
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-21 18:51:39 -0700

    Update momentInspectorHostIntelligenceStack.types.ts

--- app/components/player/momentInspectorHostIntelligenceStack.types.ts
+++ app/components/player/momentInspectorHostIntelligenceStack.types.ts
@@ -1,26 +1,9 @@
-"use client";
-
-import MomentInspectorHostFilterBar from "./MomentInspectorHostFilterBar";
-import MomentInspectorPinnedBar from "./MomentInspectorPinnedBar";
-import MomentInspectorSelectedFamilyPinBar from "./MomentInspectorSelectedFamilyPinBar";
-import MomentInspectorBookmarksBar from "./MomentInspectorBookmarksBar";
-import MomentInspectorCompareBar from "./MomentInspectorCompareBar";
-import MomentInspectorComparePanel from "./MomentInspectorComparePanel";
-import MomentInspectorIntelligencePanel from "./MomentInspectorIntelligencePanel";
-import type { MomentInspectorHostIntelligenceStackProps } from "./momentInspectorHostIntelligenceStack.types";
-
-export default function MomentInspectorHostIntelligenceStack(
-  props: MomentInspectorHostIntelligenceStackProps
-) {
-  return (
-    <>
-      <MomentInspectorHostFilterBar {...props.filterProps} />
-      <MomentInspectorPinnedBar {...props.pinnedProps} />
-      <MomentInspectorSelectedFamilyPinBar {...props.pinProps} />
-      <MomentInspectorBookmarksBar {...props.bookmarkProps} />
-      <MomentInspectorCompareBar {...props.compareBarProps} />
-      <MomentInspectorComparePanel {...props.comparePanelProps} />
-      <MomentInspectorIntelligencePanel {...props.intelligenceProps} />
-    </>
-  );
-}
+export type MomentInspectorHostIntelligenceStackProps = {
+  filterProps?: any;
+  pinnedProps?: any;
+  pinProps?: any;
+  bookmarkProps?: any;
+  compareBarProps?: any;
+  comparePanelProps?: any;
+  intelligenceProps?: any;
+};
