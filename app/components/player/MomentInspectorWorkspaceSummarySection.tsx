5dde29701f6196a0c9db5039903c460fc12e6f46
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-21 18:10:38 -0700

    Update MomentInspectorWorkspaceSummarySection.tsx

--- app/components/player/MomentInspectorWorkspaceSummarySection.tsx
+++ app/components/player/MomentInspectorWorkspaceSummarySection.tsx
@@ -12,12 +12,12 @@ export default function MomentInspectorWorkspaceSummarySection(
   props: MomentInspectorWorkspaceSummarySectionProps
 ) {
   const LegendAny = MomentInspectorWorkspaceLegend as any;
-  const summary = props.summary ?? ({} as any);
+  const summary = (props.summary ?? {}) as any;
 
   return (
     <div className="space-y-3">
-      <MomentInspectorWorkspaceSummaryBar metrics={summary.metrics ?? []} />
-      <LegendAny lanes={summary.lanes ?? []} />
+      <MomentInspectorWorkspaceSummaryBar metrics={summary?.metrics ?? []} />
+      <LegendAny lanes={summary?.lanes ?? []} />
     </div>
   );
 }
