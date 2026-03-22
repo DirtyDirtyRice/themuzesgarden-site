515c9c35c77afd9cfe03be62ea216d641df7827d
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-21 19:02:08 -0700

    Update momentInspectorHostSummaryStack.types.ts

--- app/components/player/momentInspectorHostSummaryStack.types.ts
+++ app/components/player/momentInspectorHostSummaryStack.types.ts
@@ -1,22 +1,7 @@
-"use client";
-
-import MomentInspectorSummary from "./MomentInspectorSummary";
-import MomentInspectorDiscoveryStatus from "./MomentInspectorDiscoveryStatus";
-import MomentInspectorSimilarityPanel from "./MomentInspectorSimilarityPanel";
-import MomentInspectorTagPanels from "./MomentInspectorTagPanels";
-import MomentInspectorDiagnosticsPanel from "./MomentInspectorDiagnosticsPanel";
-import type { MomentInspectorHostSummaryStackProps } from "./momentInspectorHostSummaryStack.types";
-
-export default function MomentInspectorHostSummaryStack(
-  props: MomentInspectorHostSummaryStackProps
-) {
-  return (
-    <>
-      <MomentInspectorSummary {...props.summaryProps} />
-      <MomentInspectorDiscoveryStatus {...props.discoveryProps} />
-      <MomentInspectorSimilarityPanel {...props.similarityProps} />
-      <MomentInspectorTagPanels {...props.tagProps} />
-      <MomentInspectorDiagnosticsPanel {...props.diagnosticsProps} />
-    </>
-  );
-}
+export type MomentInspectorHostSummaryStackProps = {
+  summaryProps?: any;
+  discoveryProps?: any;
+  similarityProps?: any;
+  tagProps?: any;
+  diagnosticsProps?: any;
+};
