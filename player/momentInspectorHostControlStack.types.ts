6220342f5e65a190ef57c4973cf51c9d9a7d1e9e
Steve Rice <muzesgarden@gmail.com>
2026-03-23 16:40:09 -0700

    fix host control stack import

--- player/momentInspectorHostControlStack.types.ts
+++ player/momentInspectorHostControlStack.types.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import MomentInspectorTrackControls from "./MomentInspectorTrackControls";
-import type { MomentInspectorHostControlStackProps } from "./momentInspectorHostControlStack.types";
+import type MomentInspectorHostControlStackProps from "./momentInspectorHostControlStack.types";
 
 export default function MomentInspectorHostControlStack(
   props: MomentInspectorHostControlStackProps
