cbccaef02e274ef5f8661cdcaa5e029c3cc77cc2
Steve Rice <muzesgarden@gmail.com>
2026-03-23 14:44:23 -0700

    fix workspace queue block typing

--- player/MomentInspectorWorkspacePanelQueueBlock.tsx
+++ player/MomentInspectorWorkspacePanelQueueBlock.tsx
@@ -1,35 +1,12 @@
 "use client";
 
+import type { ComponentProps } from "react";
 import MomentInspectorWorkspaceQueueBody from "./MomentInspectorWorkspaceQueueBody";
-import type { MomentInspectorWorkspaceLane } from "./momentInspectorWorkspace.types";
-import type { MomentInspectorWorkspacePanelViewModel } from "./momentInspectorWorkspacePanelViewModel";
 
-type Props = {
-  lane: MomentInspectorWorkspaceLane;
-  searchQuery: string;
-  viewModel: MomentInspectorWorkspacePanelViewModel;
-  selectedFamilyIds: string[];
-  onToggleSelected: (familyId: string) => void;
-};
+type Props = ComponentProps<typeof MomentInspectorWorkspaceQueueBody>;
 
 export default function MomentInspectorWorkspacePanelQueueBlock(
   props: Props
 ) {
-  const {
-    lane,
-    searchQuery,
-    viewModel,
-    selectedFamilyIds,
-    onToggleSelected,
-  } = props;
-
-  return (
-    <MomentInspectorWorkspaceQueueBody
-      lane={lane}
-      searchQuery={searchQuery}
-      viewModel={viewModel}
-      selectedFamilyIds={selectedFamilyIds}
-      onToggleSelected={onToggleSelected}
-    />
-  );
+  return <MomentInspectorWorkspaceQueueBody {...props} />;
 }
