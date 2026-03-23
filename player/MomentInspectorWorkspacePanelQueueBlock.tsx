5a4c4d6dfc64bd57a48558c3a4de7492a30dd310
Steve Rice <muzesgarden@gmail.com>
2026-03-23 14:40:47 -0700

    fix workspace queue block props

--- player/MomentInspectorWorkspacePanelQueueBlock.tsx
+++ player/MomentInspectorWorkspacePanelQueueBlock.tsx
@@ -1,5 +1,35 @@
 "use client";
 
-export default function MomentInspectorWorkspacePanelQueueBlock() {
-  return null;
+import MomentInspectorWorkspaceQueueBody from "./MomentInspectorWorkspaceQueueBody";
+import type { MomentInspectorWorkspaceLane } from "./momentInspectorWorkspace.types";
+import type { MomentInspectorWorkspacePanelViewModel } from "./momentInspectorWorkspacePanelViewModel";
+
+type Props = {
+  lane: MomentInspectorWorkspaceLane;
+  searchQuery: string;
+  viewModel: MomentInspectorWorkspacePanelViewModel;
+  selectedFamilyIds: string[];
+  onToggleSelected: (familyId: string) => void;
+};
+
+export default function MomentInspectorWorkspacePanelQueueBlock(
+  props: Props
+) {
+  const {
+    lane,
+    searchQuery,
+    viewModel,
+    selectedFamilyIds,
+    onToggleSelected,
+  } = props;
+
+  return (
+    <MomentInspectorWorkspaceQueueBody
+      lane={lane}
+      searchQuery={searchQuery}
+      viewModel={viewModel}
+      selectedFamilyIds={selectedFamilyIds}
+      onToggleSelected={onToggleSelected}
+    />
+  );
 }
