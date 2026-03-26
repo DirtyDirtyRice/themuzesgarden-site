58959d016b291f8f9af9f916b9a39ba36ab1ec29
DirtyDirtyRice <muzesgarden@gmail.com>
2026-03-26 14:15:58 -0700

    Update playerMomentFamilyEngine.ts

--- player/playerMomentFamilyEngine.ts
+++ player/playerMomentFamilyEngine.ts
@@ -444,7 +444,7 @@ export function buildMomentFamilies(
     );
 
     const averageDifferencePercent = Number(
-      average(memberRows.map((member) => getMemberDifferencePercent(member))).toFixed(2)
+      average(memberRows.map((member) => getMemberDifferencePercent(member))).toFixed(2))
     );
 
     const builtFamily: MomentFamilyEngineFamily = {
